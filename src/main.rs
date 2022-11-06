703c860b41f31a6a710b909130c382a06eedaa09
Philip Ye <yestyle@gmail.com>
2022-11-06 23:10:14 +1300

    Search pattern in kernel image

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
 use clap::{Arg, Command};
+use std::{
+    fs::File,
+    io::{ErrorKind, Read},
+};
+
+fn scan_term(file: &mut File, pattern: &[u8]) -> Result<usize, ErrorKind> {
+    let filelen = file.metadata().unwrap().len();
+    let mut start = 0;
+    let mut offset = 0;
+    for b in file.bytes() {
+        if let Ok(b) = b {
+            if b == pattern[start] {
+                start += 1;
+                if start == pattern.len() {
+                    // let offset point to the start of the term
+                    offset -= start - 2;
+                    break;
+                }
+            } else {
+                start = 0;
+            }
+        }
+        offset += 1;
+    }
+
+    if offset < filelen as usize - pattern.len() {
+        Ok(offset)
+    } else {
+        Err(ErrorKind::NotFound)
+    }
+}
 
 fn main() {
     let matches = Command::new(env!("CARGO_BIN_NAME"))
@@ -8,5 +39,11 @@ fn main() {
         .get_matches();
 
     let image = matches.get_one::<String>("image").unwrap();
-    println!("{image}");
+    let mut file = File::open(image).unwrap();
+    let mut pattern = "IKCFG_ST".to_string().as_bytes().to_vec();
+    pattern.extend_from_slice(&[0x1f, 0x8b, 0x08]);
+
+    if let Ok(offset) = scan_term(&mut file, &pattern) {
+        println!("{offset}");
+    }
 }
