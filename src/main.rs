f30cf35287e780767aca7805085ec2409c1b145f
Philip Ye <yestyle@gmail.com>
2022-12-22 11:17:15 +1300

    Revert "Remove unused searching functions and their dependencies"
    This reverts commit b86e87763670b70fccc503388955455988690b14.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bzip2::bufread::BzDecoder;
 use clap::{Arg, Command};
 use flate2::bufread::GzDecoder;
+use grep_matcher::Matcher;
+use grep_regex::RegexMatcherBuilder;
+use grep_searcher::{Searcher, Sink, SinkMatch};
 use lz4_flex::frame::FrameDecoder as Lz4Decoder;
 use lzma::LzmaReader;
 use regex::bytes::RegexBuilder;
@@ -23,6 +26,83 @@ const MAGIC_NUMBER_LZMA: &str = r"\x5d\x00\x00\x00";
 const MAGIC_NUMBER_LZOP: &str = r"\x89\x4c\x5a";
 const MAGIC_NUMBER_LZ4: &str = r"\x02\x21\x4c\x18";
 const MAGIC_NUMBER_ZSTD: &str = r"\x28\xb5\x2f\xfd";
+
+#[allow(dead_code)]
+fn search_bytes(file: &mut File, pattern: &[u8]) -> Result<u64, io::Error> {
+    let filelen = file.metadata()?.len();
+    let mut start = 0;
+    let mut offset: u64 = 0;
+
+    file.seek(SeekFrom::Start(0))?;
+    for b in file.bytes() {
+        if let Ok(b) = b {
+            if b == pattern[start] {
+                start += 1;
+                if start == pattern.len() {
+                    // let offset point to the start of the pattern
+                    offset -= start as u64 - 1;
+                    break;
+                }
+            } else {
+                start = 0;
+            }
+        }
+        offset += 1;
+    }
+
+    if offset < filelen - pattern.len() as u64 {
+        Ok(offset)
+    } else {
+        Err(io::Error::from(ErrorKind::NotFound))
+    }
+}
+
+struct Offset<F>(F)
+where
+    F: FnMut(u64, &[u8]) -> Result<bool, io::Error>;
+
+impl<F> Sink for Offset<F>
+where
+    F: FnMut(u64, &[u8]) -> Result<bool, io::Error>,
+{
+    type Error = io::Error;
+
+    fn matched(&mut self, _searcher: &Searcher, mat: &SinkMatch<'_>) -> Result<bool, io::Error> {
+        // mat.absolute_bytes_offset() is the offset of the matched line
+        // mat.bytes() is the bytes of the matched line
+        (self.0)(mat.absolute_byte_offset(), mat.bytes())
+    }
+}
+
+#[allow(dead_code)]
+fn search_ripgrep(file: &mut File, pattern: &str) -> Result<u64, io::Error> {
+    // Disable Unicode (\u flag) to search arbitrary (non-UTF-8) bytes
+    let matcher = if let Ok(matcher) = RegexMatcherBuilder::new().unicode(false).build(pattern) {
+        matcher
+    } else {
+        return Err(io::Error::from(ErrorKind::InvalidInput));
+    };
+
+    let mut offset = 0;
+    file.seek(SeekFrom::Start(0))?;
+    Searcher::new().search_file(
+        &matcher,
+        file,
+        Offset(|line_offset, bytes| {
+            // find pattern within the line and add onto line offset
+            // We are guaranteed to find a match, so the unwrap is OK.
+            let mymatch = matcher.find(bytes).unwrap().unwrap();
+            offset = line_offset + mymatch.start() as u64;
+            Ok(true)
+        }),
+    )?;
+
+    if offset != 0 {
+        Ok(offset)
+    } else {
+        Err(io::Error::from(ErrorKind::NotFound))
+    }
+}
 
 fn search_regex(file: &File, pattern: &str) -> Result<u64, io::Error> {
     let mut buff = BufReader::new(file);
